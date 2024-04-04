234499b9a676b8fc05dd33ce74e5c32138fc73c6
vikas.m <vikas.m@pacewisdom.com>
2024-04-04 12:03:58 +0530

    Db connection

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// require("./sequelize");
+
+const { Sequelize } = require("sequelize");
+
+console.log(process.env.DB_CONNECTION_STRING);
+const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING, {
+	logging: process.env.NODE_ENV == "production" ? false : console.log,
+});
+
+sequelize
+	.authenticate()
+	.then((_) => {
+		console.log("DB connected");
+	})
+	.catch((err) => {
+		console.log("DB connection failed with error ", err);
+	});
+
+module.exports = sequelize;
